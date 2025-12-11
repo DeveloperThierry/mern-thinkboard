65573833353cce2370b01e0e8da81d2bf36d33ca
developerthierry <developerthierry@gmail.com>
2025-12-11 23:59:19 +0000

    Middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,24 @@
-import express from "express"
-import notesRoutes from "./routes/notesRoutes.js"
-import { connectDB } from "./config/db.js"
-import dotenv from "dotenv"
-dotenv.config()
-const app = express()
-const PORT = process.env.PORT || 5050
-connectDB();
+import express from "express";
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
-app.use("/api/notes", notesRoutes)
-//middleware
-app.use(express.json())
-app.listen(PORT, ()=>{
-    console.log('server running on port', PORT)
-})
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5050;
 
+app.use("/api/notes", notesRoutes);
+
+app.use(express.json()); //allows JSOn to be parsed
+// app.use(rateLimiter)
+//custom middleware
+// app.use((req, res)=> {
+//     console.log("Request sent successfully")
+//     next()
+// })
+connectDB.then(() => {
+  app.listen(PORT, () => {
+    console.log("server running on port", PORT);
+  });
+});
