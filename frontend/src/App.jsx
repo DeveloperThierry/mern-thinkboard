7be2bd3605ea7a30db9574c87afb49fa05fe481f
developerthierry <developerthierry@gmail.com>
2025-12-12 03:02:19 +0000

    Create Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import HomePage from './pages/HomePage'
-import CreatePage from './pages/CreatePage'
-import NoteDetailPage from './pages/NoteDetailPage'
-import { Routes, Route } from 'react-router'
-import toast from 'react-hot-toast'
-
+import React from "react";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
+import { Routes, Route } from "react-router";
+import toast from "react-hot-toast";
+import daisyui from "daisyui";
+import Navbar from "./components/Navbar";
 const App = () => {
   return (
-    <div>
-      <button onClick={()=>toast.success("congrats")} className="text-green-300 p-4 bg-green-800 rounded m-4">Success</button>
+    <div data-theme="forest" className="min-h-screen">
+     <Navbar/>
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/create' element={<CreatePage />} />
-        <Route path='/note/:id' element={<NoteDetailPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
